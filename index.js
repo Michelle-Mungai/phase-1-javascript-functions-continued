1923b89eb4ef14758ca0ddba6cfb76dfe9ff86a3
Michelle-Mungai <chellymungai@gmail.com>
2022-11-25 13:59:18 +0300

    Javascript functions and interpolation

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(Event = "roller-skate"){
+    return `This Saturday, I want to ${Event}!`
+    }
+    let mondayWork = function(Event = "go to the office"){
+        return `This Monday, I will ${Event}.`
+    }
+    let wrapAdjective = function(Event = "*"){
+        return function(adjective = "hard"){
+            return `You are ${Event}${adjective}${Event}!`
+        }
+
+}
